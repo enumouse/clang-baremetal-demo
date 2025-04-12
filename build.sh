1bac1e9d94ce724f141f4abb2f1eb8643cf0ce04
Calamari <90973461+scruffykat@users.noreply.github.com>
2025-04-12 18:27:19 +0100

    Fix oopsies

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mkdir -p bin
-clang -target i386-none-elf -ffreestanding -c -o bin/kernel.o sys/kernel.c
-clang -target i386-none-elf -ffreestanding -c -o bin/i686-arch.o arch/i686/arch.S
+clang -target i686-none-elf -ffreestanding -c -o bin/kernel.o sys/kernel.c
+clang -target i686-none-elf -ffreestanding -c -o bin/i686-arch.o arch/i686/arch.S
 ld.lld -T arch/i686/linker.ld -o bin/kernel.elf bin/kernel.o bin/i686-arch.o
